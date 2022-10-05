4f74e622152b66f459bff2321addb5353571db6b
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-10-05 14:46:28 +0300

    add css style

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,10 @@
+// export default async function fetchSearchQuery(query, currentPage) {
+//   const API_KEY = `29486928-40983179e54322116410ec482`;
+//   // axios.defaults.baseURL =
+//   //   'https://pixabay.com/api/?key=29486928-40983179e54322116410ec482';
+//   return await axios.get(
+//     `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`
+//   );
+// }
+
+export const API_KEY = `29486928-40983179e54322116410ec482`;
