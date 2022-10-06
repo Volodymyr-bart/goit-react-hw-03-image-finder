686a61be09fcf1cd79b775bc0daa098ec0a137cf
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-10-06 16:38:47 +0300

    create modal window

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,32 @@
-export const App = () => {
-  return <>React homework template</>;
-};
+import { Component } from 'react';
+import { ModalWindow } from './Modal/Modal';
+export class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+  render() {
+    const { showModal } = this.state;
+    return (
+      <>
+        <button type="button" onClick={this.toggleModal}>
+          Open Modal
+        </button>
+        {showModal && (
+          <ModalWindow>
+            <h1>This is modal</h1>
+            {/* <img src="" alt="" /> */}
+            <button type="button" onClick={this.toggleModal}>
+              Close Modal
+            </button>
+          </ModalWindow>
+        )}
+      </>
+    );
+  }
+}
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,23 @@
-<div class="overlay">
-  <div class="modal">
-    <img src="" alt="" />
-  </div>
-</div>;
+// import { render } from "@testing-library/react";
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import { Modal, Overlay } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class ModalWindow extends Component {
+  componentDidMount() {
+    console.log('Modal mount');
+  }
+  componentWillUnmount() {
+    console.log('Modal anmount');
+  }
+  render() {
+    return createPortal(
+      <Overlay>
+        <Modal>{this.props.children}</Modal>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
