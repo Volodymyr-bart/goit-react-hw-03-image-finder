fdee2cc52231ee34872da5882c403b53c780bbac
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-10-07 19:45:55 +0300

    create form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
 import { Component } from 'react';
+import { SearchBar } from './Searchbar/Searchbar';
 import { ModalWindow } from './Modal/Modal';
+
 export class App extends Component {
   state = {
     showModal: false,
   };
+
+  async componentDidMount() {
+    // async function fetchSearchQuery(query, currentPage) {
+    //   const API_KEY = `29486928-40983179e54322116410ec482`;
+    //   // axios.defaults.baseURL =
+    //   //   'https://pixabay.com/api/?key=29486928-40983179e54322116410ec482';
+    //   return await axios.get(
+    //     `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`
+    //   );
+  }
 
   toggleModal = () => {
     this.setState(({ showModal }) => ({
@@ -14,6 +26,7 @@ export class App extends Component {
     const { showModal } = this.state;
     return (
       <>
+        <SearchBar />
         <button type="button" onClick={this.toggleModal}>
           Open Modal
         </button>
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,3 +1,6 @@
+// import { Formik } from 'formik';
+// import * as yup from 'yup';
+// import PropTypes from 'prop-types';
 // export default async function fetchSearchQuery(query, currentPage) {
 //   const API_KEY = `29486928-40983179e54322116410ec482`;
 //   // axios.defaults.baseURL =
@@ -7,20 +10,63 @@
 //   );
 // }
 
-export const API_KEY = `29486928-40983179e54322116410ec482`;
+import {
+  Searchbar,
+  SearchForm,
+  SearchFormButton,
+  SearchFormInput,
+  // SearchFormSpan,
+} from './Searchbar.styled';
 
-<header class="searchbar">
-  <form class="form">
-    <button type="submit" class="button">
-      <span class="button-label">Search</span>
-    </button>
+import { GoSearch } from 'react-icons/go';
 
-    <input
-      class="input"
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
-    />
-  </form>
-</header>;
+// export const API_KEY = `29486928-40983179e54322116410ec482`;
+// const schema = yup.object().shape({
+//   name: yup.string().required(),
+// });
+// const initialValues = {
+//   name: '',
+// };
+
+export const SearchBar = () => {
+  return (
+    <Searchbar>
+      <SearchForm>
+        <SearchFormButton type="submit">
+          <GoSearch />
+          {/* <SearchFormSpan>Search</SearchFormSpan> */}
+        </SearchFormButton>
+
+        <SearchFormInput
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </Searchbar>
+  );
+};
+
+// <header class="searchbar">
+//       <Formik initialValues={initialValues} validationSchema={schema}>
+//         <Formik autoComplete="off">
+//           <button type="submit" class="button">
+//             <span class="button-label">Search</span>
+//           </button>
+//           <label htmlFor="name">
+//             Name
+//             <input
+//               // class="input"
+//               type="text"
+//               name="name"
+//               autocomplete="off"
+//               placeholder="Search images and photos"
+//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//             />
+//             {/* <Error name="name" /> */}
+//           </label>
+//         </Formik>
+//       </Formik>
+//     </header>
